28472b88b6c5ff20d8c70e41887e2949d23fea86
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-06-16 12:05:04 +0330

    IMarkerProps.ts has incorrect type which was fixed.

--- src/interfaces/IMarkerProps.ts
+++ src/interfaces/IMarkerProps.ts
@@ -2,6 +2,6 @@ import mapboxgl from "mapbox-gl";
 import IChildrenProps from "./IChildrenProps";
 
 export default interface IMarkerProps extends IChildrenProps {
-  lngLat?: mapboxgl.LngLat;
+  lngLat?: mapboxgl.LngLatLike;
   color?: string;
 }
