78ca8df5185f68f4a387ef7996625d2ae3eee6bf
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-06-17 10:04:35 +0330

    The problem with layer change by source is currently resolved.

--- src/components/Layer.tsx
+++ src/components/Layer.tsx
@@ -13,38 +13,36 @@ const Layer = ({
   ...rest
 }: LayerPropsType) => {
   React.useEffect(() => {
-    // const { layout, paint, filter } = rest as mapboxgl.Layer;
+    const { layout, paint, filter } = rest as mapboxgl.Layer;
 
     const callback: QueueCallbackType = (map) => {
-      map.addLayer(rest);
+      if (!map.getLayer(rest.id)) {
+        map.addLayer(rest);
+      } else {
+        if (layout) {
+          for (const layoutKey in layout) {
+            map.setLayoutProperty(
+              rest.id,
+              layoutKey,
+              layout[layoutKey as keyof typeof layout]
+            );
+          }
+        }
 
-      // if (!map.getLayer(rest.id)) {
-      //   map.addLayer(rest);
-      // } else {
-      //   if (layout) {
-      //     for (const layoutKey in layout) {
-      //       map.setLayoutProperty(
-      //         rest.id,
-      //         layoutKey,
-      //         layout[layoutKey as keyof typeof layout]
-      //       );
-      //     }
-      //   }
-      //
-      //   if (paint) {
-      //     for (const key in paint) {
-      //       map.setPaintProperty(
-      //         rest.id,
-      //         key,
-      //         paint[key as keyof typeof paint]
-      //       );
-      //     }
-      //   }
-      //
-      //   if (filter) {
-      //     map.setFilter(rest.id, filter);
-      //   }
-      // }
+        if (paint) {
+          for (const key in paint) {
+            map.setPaintProperty(
+              rest.id,
+              key,
+              paint[key as keyof typeof paint]
+            );
+          }
+        }
+
+        if (filter) {
+          map.setFilter(rest.id, filter);
+        }
+      }
 
       if (onClick) {
         map.off("click", rest.id, onClick);
