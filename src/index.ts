ebf25853142b07b56720e8747459bf2b9d400078
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-07-07 22:51:13 +0330

    The `onSourceData` event was added.

--- src/index.ts
+++ src/index.ts
@@ -22,4 +22,5 @@ export {
   HeatmapLayer,
   GeoJSONSource,
 };
+
 export type { MapStyleType };
--- src/interfaces/IMapEvents.ts
+++ src/interfaces/IMapEvents.ts
@@ -11,5 +11,6 @@ export default interface IMapEvents {
   onClick?: (event: mapboxgl.MapMouseEvent) => void;
   onMoveEnd?: (event: mapboxgl.MapboxEvent) => void;
   onStyleLoad?: (event: mapboxgl.MapboxEvent) => void;
-  onSourceData?: (event: mapboxgl.MapboxEvent) => void;
+  onStyleData?: (event: mapboxgl.MapStyleDataEvent) => void;
+  onSourceData?: (event: mapboxgl.MapSourceDataEvent) => void;
 }
